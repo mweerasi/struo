a312ab3b793ef60f734d4b907c240a09f73d9024
Manju Weerasinghe <manju@Manjus-MacBook-Pro.local>
2017-01-03 20:29:34 -0500

    retrieve weather information class

--- app/src/main/java/arkm/struo/Functions.java
+++ app/src/main/java/arkm/struo/Functions.java
@@ -103,9 +103,37 @@ public class Functions {
             } catch (JSONException e) {
                 //Log.e(LOG_TAG, "Cannot process JSON results", e);
             }
+        }
 
+        public static JSONObject getWeatherJSON(String lat, String lon){
+            try {
+                URL url = new URL(String.format(OPEN_WEATHER_MAP_URL, lat, lon));
+                HttpURLConnection connection =
+                        (HttpURLConnection)url.openConnection();
 
+                connection.addRequestProperty("x-api-key", OPEN_WEATHER_MAP_API);
 
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(connection.getInputStream()));
+
+                StringBuffer json = new StringBuffer(1024);
+                String tmp="";
+                while((tmp=reader.readLine())!=null)
+                    json.append(tmp).append("\n");
+                reader.close();
+
+                JSONObject data = new JSONObject(json.toString());
+
+                // This value will be 404 if the request was not
+                // successful
+                if(data.getInt("cod") != 200){
+                    return null;
+                }
+
+                return data;
+            }catch(Exception e){
+                return null;
+            }
         }
 
 
