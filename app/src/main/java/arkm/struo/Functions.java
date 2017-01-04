1d577ba04027be4264738565044eea67ce50b7a6
Manju Weerasinghe <manju@Manjus-MacBook-Pro.local>
2017-01-03 20:26:47 -0500

    post execute class, set and retrive main variables

--- app/src/main/java/arkm/struo/Functions.java
+++ app/src/main/java/arkm/struo/Functions.java
@@ -25,11 +25,14 @@ public class Functions {
     public static String setWeatherIcon(int actualId, long sunrise, long sunset){
         int id = actualId / 100;
         String icon = "";
-        if(actualId == 800){
+        if(actualId == 800) {
             long currentTime = new Date().getTime();
-            if(currentTime>=sunrise && currentTime < sunset){
+            if (currentTime >= sunrise && currentTime < sunset) {
                 icon = "&#xf00d;";
             } else {
+                icon = "&#xf02e;";
+            }
+        } else {
                 switch(id){
                     case 2 : icon = "&#xf01e;";
                         break;
@@ -47,6 +50,65 @@ public class Functions {
             }
             return icon;
         }
+
+        public interface AsyncResponse{
+            void processFinish(String output1, String output2, String output3, String output4, String output5, String output6, String output7, String output8);
+
+        }
+
+    public static class placeIdTask extends AsyncTask<String, Void, JSONObject> {
+
+        public AsyncResponse delegate = null;//Call back interface
+
+        public placeIdTask(AsyncResponse asyncResponse) {
+            delegate = asyncResponse;//Assigning call back interfacethrough constructor
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+            JSONObject jsonWeather = null;
+            try {
+                jsonWeather = getWeatherJSON(params[0], params[1]);
+            } catch (Exception e) {
+                Log.d("Error", "Cannot process JSON results", e);
+            }
+
+
+            return jsonWeather;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject json) {
+            try {
+                if(json != null){
+                    JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+                    JSONObject main = json.getJSONObject("main");
+                    DateFormat df = DateFormat.getDateTimeInstance();
+
+
+                    String city = json.getString("name").toUpperCase(Locale.CANADA) + ", " + json.getJSONObject("sys").getString("country");
+                    String description = details.getString("description").toUpperCase(Locale.CANADA);
+                    String temperature = String.format("%.2f", main.getDouble("temp"))+ "°";
+                    String humidity = main.getString("humidity") + "%";
+                    String pressure = main.getString("pressure") + " hPa";
+                    String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+                    String iconText = setWeatherIcon(details.getInt("id"),
+                            json.getJSONObject("sys").getLong("sunrise") * 1000,
+                            json.getJSONObject("sys").getLong("sunset") * 1000);
+
+                    delegate.processFinish(city, description, temperature, humidity, pressure, updatedOn, iconText, ""+ (json.getJSONObject("sys").getLong("sunrise") * 1000));
+
+                }
+            } catch (JSONException e) {
+                //Log.e(LOG_TAG, "Cannot process JSON results", e);
+            }
+
+
+
+        }
+
+
     }
 
 }
